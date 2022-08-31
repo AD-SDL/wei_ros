2d9d13f283a98ee36699ed134a41df99b0eaa43b
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-31 16:36:18 -0500

    Testing install script changes to install rpl_wei as package

--- examples/install.sh
+++ examples/install.sh
@@ -1,16 +1,17 @@
-
-
-
-
 ROS_WS=~/wei_ws
 
+mkdir $ROS_WS
 mkdir $ROS_WS/src
 cd $ROS_WS/src
-git clone https://github.com/AD-SDL/azenta_driver 
+git clone https://github.com/AD-SDL/azenta_driver
 git clone https://github.com/AD-SDL/hudson_driver
 git clone https://github.com/AD-SDL/pf400_driver
 git clone https://github.com/AD-SDL/wei_ros
 git clone https://github.com/AD-SDL/rpl_wei
+cd $ROS_WS/src/rpl_wei
+pip3 install -r requirements/requirements.txt
+pip3 install -e .
+cd $ROS_WS/src
 git clone https://github.com/AD-SDL/rpl_workcell
 cd ..
-colcon build 
+colcon build

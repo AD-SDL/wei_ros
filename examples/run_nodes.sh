7c74484f06e209f8d2bc62afb5c0fea3e2e12f0b
Kyle Hippe <kyle.hippe@gmail.com>
2022-09-01 10:48:07 -0500

    Testing install and run nodes

--- examples/run_nodes.sh
+++ examples/run_nodes.sh
@@ -1,7 +1,7 @@
+#!bin/bash
 
-
-
-source ~/wei_ws/install/setup.bash & 
+source ~/wei_ws/install/setup.bash &
 ros2 launch sp_module_client sp_module.launch.py &
-ros2 launch pf400_client pf400_client.launch.py & 
-ros2 launch sciclops_module_client sciclops_module.launch.py
+ros2 launch pf400_client pf400_client.launch.py &
+ros2 launch sciclops_module_client sciclops_module.launch.py &
+wait

d2f633daf02c98eb149421d488d35f4f0634d109
Kyle Hippe <kyle.hippe@gmail.com>
2022-09-01 11:43:15 -0500

    Testing week 2 test

--- examples/install.sh
+++ examples/install.sh
@@ -5,9 +5,9 @@ ROS_WS=~/wei_ws_dev_kyle
 mkdir $ROS_WS
 mkdir $ROS_WS/src
 cd $ROS_WS/src
-git clone https://github.com/AD-SDL/azenta_driver
-cd $ROS_WS/src/azenta_driver/
-git checkout 55d4598
+# git clone https://github.com/AD-SDL/azenta_driver
+# cd $ROS_WS/src/azenta_driver/
+# git checkout 55d4598
 cd $ROS_WS/src
 git clone https://github.com/AD-SDL/hudson_driver
 git clone https://github.com/AD-SDL/pf400_driver

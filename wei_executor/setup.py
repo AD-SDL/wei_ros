b0f6d263969a236db7c5854db2300f58cc587754
Rafael Vescovi <ravescovi@anl.gov>
2022-10-25 16:50:59 -0500

    fix

--- wei_executor/setup.py
+++ wei_executor/setup.py
@@ -5,7 +5,6 @@ package_name = 'wei_executor'
 setup(
     name=package_name,
     version='0.0.0',
-
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -14,8 +13,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='decarabas',
-    maintainer_email='rpl@todo.todo',
+    maintainer='Rafael Vescovi',
+    maintainer_email='ravescovi@anl.gov',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],

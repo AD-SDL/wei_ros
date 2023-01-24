6f7f86fdf3cd6161c626677e621aba0bf63fd2b7
Dozgulbas <dozgulbas@anl.gov>
2023-01-24 17:28:39 -0600

    Clean up

--- wei_executor/wei_executor/weiExecutorNode.py
+++ wei_executor/wei_executor/weiExecutorNode.py
@@ -43,7 +43,7 @@ class weiExecNode(Node):
         print(res.description)
         return res.description
 
-    def capture_image(self, image_stream, image_name = "/camera_image.png", path = "~/", node_name = "CameraPublisher1"):
+    def capture_image(self, image_stream, image_name = "camera_image.png", path = "~/", node_name = "CameraPublisher1"):
         self.image_path = os.path.join(path, image_name)
 #        node = rclpy.create_node('image_subscriber')
         self.create_subscription(Image, "/std_ns/" + node_name + "/video_frames", self.save_image_callback, qos_profile_sensor_data)

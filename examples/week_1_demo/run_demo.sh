124e1681e2a12455a2d67bf85e4bdf027e096163
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-01-25 12:35:59 -0600

    cleanup

--- examples/week_1_demo/run_demo.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-source ~/wei_ws_dev_kyle/install/setup.bash &
-python3 ~/wei_ws_dev_kyle/src/wei_ros/examples/test_wei_wf.py -wc ~/wei_ws_dev_kyle/src/rpl_workcell/pcr_workcell/pcr_workcell.yaml -wf ~/wei_ws_dev_kyle/src/rpl_workcell/pcr_workcell/workflows/week_demo_workflow.yaml

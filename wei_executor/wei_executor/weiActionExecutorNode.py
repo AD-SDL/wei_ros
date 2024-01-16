e425f594d94c55fbde970c82ec30906c5833eaa2
Dozgulbas <dozgulbas@anl.gov>
2024-01-16 16:12:54 -0600

    Clean up

--- wei_executor/wei_executor/weiActionExecutorNode.py
+++ wei_executor/wei_executor/weiActionExecutorNode.py
@@ -63,34 +63,36 @@ class weiExecNode(Node):
     
     #-----------                                             
     
-    def send_wei_command(self,ros_node,action_handle, action_vars={}):
-        weiActionClient = self.create_client(WeiAction,ros_node+'/action_handler')
-        while not weiActionClient.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info(ros_node + ' service not available, waiting again...')
-        weiReq = WeiAction.Request()
-        weiReq.action_handle=str(action_handle)
-        weiReq.vars=json.dumps(action_vars)
-        print(weiReq.vars)
-        future = weiActionClient.call_async(weiReq)
-        rclpy.spin_until_future_complete(self, future)  
-        res = future.result()
-        print(res.action_response)
-        print(res.action_msg)
-        print(res.action_log)
-        pass
+    def send_wei_command(self, ros_node, action_handle, action_vars={}):
+        # weiActionClient = self.create_client(WeiAction,ros_node+'/action_handler')
+        # while not weiActionClient.wait_for_service(timeout_sec=1.0):
+        #     self.get_logger().info(ros_node + ' service not available, waiting again...')
+        # weiReq = WeiAction.Request()
+        # weiReq.action_handle=str(action_handle)
+        # weiReq.vars=json.dumps(action_vars)
+        # print(weiReq.vars)
+        # future = weiActionClient.call_async(weiReq)
+        # rclpy.spin_until_future_complete(self, future)  
+        # res = future.result()
+        # print(res.action_response)
+        # print(res.action_msg)
+        # print(res.action_log)
         # return res.action_response, res.action_msg, res.action_log
+
+        pass
         
     
     def get_description(self, node_name):
-        weiDescClient = self.create_client(WeiDescription,node_name+'/get_description')
-        while not weiDescClient.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info(node_name + ' service not available, waiting again...')
-        future = weiDescClient.call_async()
-        rclpy.spin_until_future_complete(self, future)  
-        res = future.result()
-        print(res.description)
-        pass
+        # weiDescClient = self.create_client(WeiDescription,node_name+'/get_description')
+        # while not weiDescClient.wait_for_service(timeout_sec=1.0):
+        #     self.get_logger().info(node_name + ' service not available, waiting again...')
+        # future = weiDescClient.call_async()
+        # rclpy.spin_until_future_complete(self, future)  
+        # res = future.result()
+        # print(res.description)
         # return res.description
+
+        pass
 
     def get_log(self, node_name):
         pass
